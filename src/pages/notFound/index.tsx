b7589bfca4fe86aed9487799d12e1d745809cd6c
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-11-10 23:45:33 +0500

    Добавляет компонент NotFoundPage

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+// import s from "./NotFound.module.scss";
+
+import Layout from "../../components/layout";
+// import NotFound from "../../components/notFound";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <Layout>
+        {/* <NotFound /> */}
+        <h1>Error 404: page not found</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default NotFoundPage;
