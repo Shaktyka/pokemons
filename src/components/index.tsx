8300bf267d8cd1de5ac5caa3b3612030f5f0fc03
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-11-14 12:02:31 +0500

    Импортирует компоненты в один файл

---
+++ src/components/index.tsx
@@ -0,0 +1,31 @@
+import App from "./app/App";
+import Burger from "./burger";
+import Button from "./button";
+import Footer from "./footer";
+import Header from "./header";
+import Heading from "./heading";
+import Layout from "./layout";
+import Menu from "./menu";
+import NotFound from "./notFound";
+import Parallax from "./parallax";
+import Pokedex from "./pokedex";
+import PokemonCard from "./pokemonCard";
+import PokemonsList from "./pokemonslist";
+import Promo from "./promo";
+
+export {
+  App,
+  Burger,
+  Button,
+  Footer,
+  Header,
+  Heading,
+  Layout,
+  Menu,
+  NotFound,
+  Parallax,
+  Pokedex,
+  PokemonCard,
+  PokemonsList,
+  Promo,
+};
