39194c3fcf8923c4031968bcd36fe531cf95988e
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-11-14 17:04:25 +0500

    Убирает правки типизации покемонов

--- src/types.ts
+++ src/types.ts
@@ -7,16 +7,16 @@ export type IStats = {
 
 // Объект данных одного покемона
 export interface IPokemon {
-  ["name_clean"]?: string;
-  abilities?: string[];
-  stats?: IStats;
-  types?: string[];
-  img?: string;
-  name?: string;
-  ["base_experience"]?: number;
-  height?: number;
-  id?: number;
-  ["is_default"]?: boolean;
-  order?: number;
-  weight?: number;
+  ["name_clean"]: string;
+  abilities: string[];
+  stats: IStats;
+  types: string[];
+  img: string;
+  name: string;
+  ["base_experience"]: number;
+  height: number;
+  id: number;
+  ["is_default"]: boolean;
+  order: number;
+  weight: number;
 }
